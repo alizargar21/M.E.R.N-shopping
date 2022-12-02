0dc13d4027f5cbd387d90d5b2bf87490fac7169a
Ali Zargar <mrzargar1995@gmail.com>
2022-12-02 13:26:28 +0330

    change command 6

--- client/src/services/httpServices.js
+++ client/src/services/httpServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
-
+console.log(process.env.REACT_APP_BASE_URL)
 const http = {
   get: axios.get,
   put: axios.put,
